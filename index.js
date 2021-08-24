f4f4c6d75e4580346c1db6a915c29263df777c39
Kailas Walldoddi <Kailas.Walldoddi@ellucian.com>
2021-08-24 09:21:30 +0530

    usage of ejs tempalte

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 const express = require('express')
 const app = express();
 const jwt = require('jsonwebtoken');
+const ejs = require('ejs');
 let users = []
 
 // to make sure you send html files with external css files
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/static'));
 // to make sure you can read psot body.
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req,res) => {
-    res.sendFile(__dirname+'/index.html')
+    res.render('index');
+    // res.sendFile(__dirname+'/index.html')
 })
 
 app.get('/users', (req,res) => {
-    res.send(users);
+    res.render('users', {
+        users: users
+    });
+    // res.send(users);
 })
 
 app.get('/signup', (req,res) => {
-    res.sendFile(__dirname+'/signup.html')
+    res.render('signup');
+    // res.sendFile(__dirname+'/signup.html')
 })
 
 app.get('/signin', (req,res) => {
-    res.sendFile(__dirname+'/signin.html')
+    res.render('signin');
+    // res.sendFile(__dirname+'/signin.html')
 })
 
 app.post('/signup', (req,res) => {
     req.body.psw = jwt.sign(req.body.psw , 'secret');
     delete req.body["psw-repeat"];
     users.push(req.body);
-    res.write("signed up with folowing details \n");
-    res.write(JSON.stringify(req.body));
-    res.end();
+    res.render('users', {
+        users: users
+    });
 })
 
 app.post('/signin', (req,res) => {
